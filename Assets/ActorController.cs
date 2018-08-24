0d85e0bc65e6e2079cd170cc9fe6820f2fc17b4a
wangyixing <wangyihang01@corp.netease.com>
2018-08-24 22:13:22 +0800

    first commit

---
+++ Assets/ActorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour {
+    public GameObject model;
+    [SerializeField] //私有变量不显示到编辑器, SerilizeFiled强制显示
+    private Animator anim;
+    public PlayerInput pi;
+
+    //尽量在awake方法中getcomponent
+    void Awake() {
+        anim = model.GetComponent<Animator>();
+        pi = GetComponent<PlayerInput>();
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetFloat("forward", pi.dirUp);
+	}
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public string keyUp, keyDown, keyLeft, keyRight;
+    public float dirUp, dirRight;
+    private float velocityUp, velocityRight;
+    public bool inputEnabled;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dirUpTarget = (Input.GetKey(keyUp)?1.0f:0) - (Input.GetKey(keyDown)?1.0f:0);
+        float dirRightTarget = (Input.GetKey(keyRight)?1.0f:0) - (Input.GetKey(keyRight)?1.0f:0);
+        print(inputEnabled.ToString());
+        if (inputEnabled == false) {
+            dirUpTarget = dirRightTarget = 0;
+        }
+
+        //平滑插值
+        dirUp = Mathf.SmoothDamp(dirUp, dirUpTarget, ref velocityUp, 0.1f);
+        dirRight = Mathf.SmoothDamp(dirRight, dirRightTarget, ref velocityRight, 0.1f);
+	}
+}
